bb4992e96b0f905a169722d941d44fc015ced9d4
Allen Jude <ajallen142001@gmail.com>
2026-07-02 20:33:50 +0530

    Added Lufga font

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'presentation/screens/root_screen.dart';
+import 'package:radix/presentation/screens/root_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  runApp(const ProviderScope(child: RadixApp()));
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class RadixApp extends StatelessWidget {
-  const RadixApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Radix',
+      title: 'Workout App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: 'Lufga', 
         scaffoldBackgroundColor: const Color(0xFF1A1A1A), 
         colorScheme: const ColorScheme.dark(
-          primary: Color(0xFFA4EB3F), 
-          surface: Color(0xFF2A2A2A), 
+          primary: Color(0xFFA4EB3F),
+          surface: Color(0xFF1A1A1A),
         ),
-        useMaterial3: true,
       ),
-      home: const RootScreen(), 
+      home: const RootScreen(),
     );
   }
 }
